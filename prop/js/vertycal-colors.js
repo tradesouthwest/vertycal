0e198815998123768fc16fd46e09a9f8a4e25bbd
Larry <tradesouthwest@outlook.com>
2024-07-15 14:10:31 -0700

    Add files via upload

---
+++ prop/js/vertycal-colors.js
@@ -0,0 +1,10 @@
+/** 
+ * WP Color Picker 
+ * @subpackage VertyCal Plugin
+ */
+jQuery(document).ready(function($) {
+	jQuery('.vertycal-color-picker-1').wpColorPicker();
+	jQuery('.vertycal-color-picker-2').wpColorPicker();
+	jQuery('.vertycal-color-picker-3').wpColorPicker();
+	//$('.vertycal-color-picker-4').wpColorPicker();
+});
